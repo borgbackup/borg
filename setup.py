ae7b0eaf6d834090a5297b2a3b057ecbf5260123
Jonas Borgström <jonas@borgstrom.se>
2010-10-30 13:24:23 +0200

    Added argparse as a dependency

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 # -*- encoding: utf-8 *-*
 #!/usr/bin/env python
-
+import sys
 from setuptools import setup, Extension
+
+dependencies = ['pycrypto', 'msgpack-python', 'pbkdf2.py'],
+if sys.version_info < (2, 7):
+    dependencies.append('argparse')
+
 
 setup(name='darc',
       version='0.1',
@@ -9,7 +14,7 @@ setup(name='darc',
       author_email='jonas@borgstrom.se',
       packages=['darc'],
       ext_modules=[Extension('darc._speedups', ['darc/_speedups.c'])],
-      install_requires=['pycrypto', 'msgpack-python', 'pbkdf2.py'],
+      install_requires=dependencies,
       entry_points = {
         'console_scripts': [
             'darc = darc.archiver:main',

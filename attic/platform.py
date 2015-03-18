767e79559784b6d1b922bd05c8984483212cab28
Thomas Waldmann <tw@waldmann-edv.de>
2015-03-18 18:21:04 +0100

    dummy acl implementation for unsupported platforms
    the API_VERSION there was not in sync (and that was even true, as "st" was missing in acl_get()) - fixed.

--- attic/platform.py
+++ attic/platform.py
@@ -9,9 +9,13 @@ elif platform == 'FreeBSD':
 elif platform == 'Darwin':
     from attic.platform_darwin import acl_get, acl_set, API_VERSION
 else:
-    API_VERSION = 1
+    # this is a dummy acl interface for platforms for which we do not have
+    # a real implementation (or which do not support acls at all).
 
-    def acl_get(path, item, numeric_owner=False):
+    API_VERSION = 2
+
+    def acl_get(path, item, st, numeric_owner=False):
         pass
+
     def acl_set(path, item, numeric_owner=False):
         pass

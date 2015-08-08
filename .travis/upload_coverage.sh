60e34968b023a3411941fa9f17f3bd2ac332fb92
Thomas Waldmann <tw@waldmann-edv.de>
2015-08-08 19:22:25 +0200

    codecov: fixes for env vars and osx

--- .travis/upload_coverage.sh
+++ .travis/upload_coverage.sh
@@ -7,5 +7,7 @@ NO_COVERAGE_TOXENVS=(pep8)
 if ! [[ "${NO_COVERAGE_TOXENVS[*]}" =~ "${TOXENV}" ]]; then
     source ~/.venv/bin/activate
     ln .tox/.coverage .coverage
-    codecov -e TRAVIS_OS_NAME,TOXENV
+    # on osx, tests run as root, need access to .coverage
+    sudo chmod 666 .coverage
+    codecov -e TRAVIS_OS_NAME TOXENV
 fi

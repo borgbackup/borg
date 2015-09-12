bc2cfdfc595508d0bdd2156f066124a282f89291
Thomas Waldmann <tw@waldmann-edv.de>
2015-09-13 01:01:48 +0200

    fix the other argparse import also

--- borg/helpers.py
+++ borg/helpers.py
@@ -1,4 +1,6 @@
-import argparse
+from .support import argparse  # see support/__init__.py docstring
+                               # DEPRECATED - remove after requiring py 3.4
+
 import binascii
 from collections import namedtuple
 from functools import wraps

3c535b9d7ab1f49e39c5b5632f97545b147b4b05
Antoine Beaupré <anarcat@koumbit.org>
2015-10-09 16:09:25 -0400

    switch to default rtd theme
    this still doesn't quite work: our sidebar is gone, so no more useful
    links and related projects. we also loose the link to github and the
    RTD popup, although the latter still needs to be confirmed on RTD
    infra

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,8 @@ sys.path.insert(0, os.path.abspath('..'))
 
 from borg import __version__ as sw_version
 
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -92,7 +94,11 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'local'
+#html_theme = ''
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the

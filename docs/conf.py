256f89e69725d12f2f7f919a440ef31ff11ae3c1
Antoine Beaupré <anarcat@koumbit.org>
2015-10-13 08:35:12 -0400

    add back logo, see #272
    it's ugly as hell, but at least we have some logo there. keeping the issue opened for improvements

--- docs/conf.py
+++ docs/conf.py
@@ -117,7 +117,7 @@ html_theme_path = ['_themes']
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = '_static/favicon.ico'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -127,7 +127,7 @@ html_favicon = 'favicon.ico'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+#html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.

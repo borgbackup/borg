d061062eb8bb228183e60dc189c09a73a16bcc80
Antoine Beaupré <anarcat@koumbit.org>
2015-10-16 08:18:07 -0400

    fixup: syntax error

--- borg/archiver.py
+++ borg/archiver.py
@@ -284,7 +284,7 @@ Type "Yes I am sure" if you understand this and want to continue.\n"""))
                 archive.extract_item(dirs.pop(-1))
         for pattern in (patterns or []):
             if isinstance(pattern, IncludePattern) and  pattern.match_count == 0:
-                self.print_error(_('Warning: Include pattern "%s" never matched.', pattern)
+                self.print_error(_('Warning: Include pattern "%s" never matched.'), pattern)
         return self.exit_code
 
     def do_rename(self, args):

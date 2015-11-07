244303ac668471ebb638f7278645a381267d239e
Thomas Waldmann <tw@waldmann-edv.de>
2015-11-07 19:55:17 +0100

    add ACL keys the RobustUnpacker must know about

--- borg/archive.py
+++ borg/archive.py
@@ -595,7 +595,7 @@ Number of files: {0.stats.nfiles}'''.format(self)
 # this set must be kept complete, otherwise the RobustUnpacker might malfunction:
 ITEM_KEYS = set([b'path', b'source', b'rdev', b'chunks',
                  b'mode', b'user', b'group', b'uid', b'gid', b'mtime', b'atime', b'ctime',
-                 b'xattrs', b'bsdflags',
+                 b'xattrs', b'bsdflags', b'acl_nfs4', b'acl_access', b'acl_default', b'acl_extended',
             ])
 
 

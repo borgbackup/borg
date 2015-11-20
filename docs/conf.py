87bb88670584f9a5abeb13c8bc96a72b73519978
Thomas Waldmann <tw@waldmann-edv.de>
2015-11-20 18:51:11 +0100

    rtd theme adjustment: borg darkness for the upper left corner

--- docs/conf.py
+++ docs/conf.py
@@ -127,7 +127,8 @@ html_favicon = '_static/favicon.ico'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
+html_static_path = ['borg_theme']
+html_style = 'css/borg.css'
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.

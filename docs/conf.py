077bcd0cdec7f54d8467749f719864f615837dd9
Thomas Waldmann <tw@waldmann-edv.de>
2015-11-20 19:06:40 +0100

    logo: have a separate, bigger logo (do not reuse the favicon)
    currently just scaled up / converted to png from the favicon.ico.

--- docs/conf.py
+++ docs/conf.py
@@ -117,7 +117,7 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '_static/favicon.ico'
+html_logo = '_static/logo.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32

3a1b83d6698815453d49e31ba8995ad68b03338b
Thomas Waldmann <tw@waldmann-edv.de>
2016-04-23 20:27:14 +0200

    msgpack / msg_pack: add some tests
    they also show where old and new code are compatible and where not.

--- borg/testsuite/msg_pack.py
+++ borg/testsuite/msg_pack.py
@@ -1,3 +1,5 @@
+import pytest
+
 import msgpack
 
 from ..msg_pack import packb, unpackb, is_slow
@@ -15,6 +17,7 @@ def test_is_slow():
 
 
 def test_msg_pack_roundtrip():
+    # this is how the new code works, easily roundtripping bytes and str
     data = b'bytes-ascii\x00'
     assert data == unpackb(packb(data))
     data = b'bytes-arbitrary-\xe4\xf6\xfc\x00'
@@ -23,3 +26,37 @@ def test_msg_pack_roundtrip():
     assert data == unpackb(packb(data))
     data = 'text-unicode-äöü\u0000'
     assert data == unpackb(packb(data))
+
+
+def test_msgpack_roundtrip():
+    # this is how the old code works (directly using msgpack)
+    # str needs addtl. decode to round-trip
+    data = 'text-unicode-äöü\u0000'
+    assert msgpack.unpackb(msgpack.packb(data)).decode('utf-8') == data
+    # bytes do roundtrip without addtl. effort
+    data = b'bytes-arbitrary-\xe4\xf6\xfc\x00'
+    assert msgpack.unpackb(msgpack.packb(data)) == data
+
+
+def test_compat_keys():
+    key_old = b'keyname'  # old code item dict key
+    key_new = 'keyname'  # new code item dict key
+    # old code with old key produces same output as new code with new key
+    assert msgpack.packb(key_old) == packb(key_new)
+
+
+def test_compat_values_str():
+    # old and new code produce same output for str
+    value = 'str-ascii'
+    assert msgpack.packb(value) == packb(value)
+    value = 'str-unicode-äöü\u0000'
+    assert msgpack.packb(value) == packb(value)
+
+
+@pytest.mark.xfail(reason="impossible, bytes must serialize differently now from str to have clean roundtrip")
+def test_compat_values_bytes():
+    # old and new code do NOT produce same output for bytes
+    value = b'bytes-ascii'
+    assert msgpack.packb(value) == packb(value)
+    value = b'bytes-arbitrary-\xe4\xf6\xfc\x00'
+    assert msgpack.packb(value) == packb(value)

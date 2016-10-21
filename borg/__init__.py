22f77b908f31cd4d0de77a16a77f868843322f34
Thomas Waldmann <tw@waldmann-edv.de>
2016-10-22 00:20:50 +0200

    implement borgmajor/minor/patch placeholders, fixes #1694

--- borg/__init__.py
+++ borg/__init__.py
@@ -1,11 +1,6 @@
-import re
+from distutils.version import LooseVersion
 
 from ._version import version as __version__
 
-version_re = r'(\d+)\.(\d+)\.(\d+)'
 
-m = re.match(version_re, __version__)
-if m:
-    __version_tuple__ = tuple(map(int, m.groups()))
-else:
-    raise RuntimeError("Can't parse __version__: %r" % __version__)
+__version_tuple__ = tuple(LooseVersion(__version__).version[:3])

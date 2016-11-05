adfd66d636e56cf21a7027e4169fcaf2d48b0197
Antoine Beaupré <anarcat@debian.org>
2016-11-04 21:27:21 -0400

    restore normal size
    otherwise code examples overflow

--- docs/conf.py
+++ docs/conf.py
@@ -193,7 +193,7 @@ htmlhelp_basename = 'borgdoc'
 #latex_paper_size = 'letter'
 
 # The font size ('10pt', '11pt' or '12pt').
-latex_font_size = '12pt'
+#latex_font_size = '12pt'
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).

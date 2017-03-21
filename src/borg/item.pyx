f7081837438da6652ed1c1056303ecf6ae6625da
Thomas Waldmann <tw@waldmann-edv.de>
2017-03-21 02:21:32 +0100

    Revert "don't do "bigint" conversion for nanosecond mtime"
    This reverts commit 8b2e7ec68099fc85ac7298d462db14b5f0ee7486.
    
    We still need the bigint stuff for borg 1.0 compatibility.
    
    # Conflicts:
    #	src/borg/cache.py

--- src/borg/item.pyx
+++ src/borg/item.pyx
@@ -2,6 +2,7 @@ from collections import namedtuple
 
 from .constants import ITEM_KEYS
 from .helpers import safe_encode, safe_decode
+from .helpers import bigint_to_int, int_to_bigint
 from .helpers import StableDict
 
 API_VERSION = '1.1_02'
@@ -156,9 +157,9 @@ class Item(PropDict):
     rdev = PropDict._make_property('rdev', int)
     bsdflags = PropDict._make_property('bsdflags', int)
 
-    atime = PropDict._make_property('atime', int)
-    ctime = PropDict._make_property('ctime', int)
-    mtime = PropDict._make_property('mtime', int)
+    atime = PropDict._make_property('atime', int, 'bigint', encode=int_to_bigint, decode=bigint_to_int)
+    ctime = PropDict._make_property('ctime', int, 'bigint', encode=int_to_bigint, decode=bigint_to_int)
+    mtime = PropDict._make_property('mtime', int, 'bigint', encode=int_to_bigint, decode=bigint_to_int)
 
     # size is only present for items with a chunk list and then it is sum(chunk_sizes)
     # compatibility note: this is a new feature, in old archives size will be missing.

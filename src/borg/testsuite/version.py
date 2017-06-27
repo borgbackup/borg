7965efd5d91111cf469b9da889619818c53a00f4
Marian Beermann <public@enkore.de>
2017-06-27 10:11:57 +0200

    version: add missing test for format_version, fix bug

--- src/borg/testsuite/version.py
+++ src/borg/testsuite/version.py
@@ -1,6 +1,6 @@
 import pytest
 
-from ..version import parse_version
+from ..version import parse_version, format_version
 
 
 @pytest.mark.parametrize("version_str, version_tuple", [
@@ -36,3 +36,18 @@ def test_parse_version_invalid():
         assert parse_version('1.2')  # we require x.y.z versions
     with pytest.raises(ValueError):
         assert parse_version('crap')
+
+
+@pytest.mark.parametrize("version_str, version_tuple", [
+    ('1.0.0a1', (1, 0, 0, -4, 1)),
+    ('1.0.0', (1, 0, 0, -1)),
+    ('1.0.0a2', (1, 0, 0, -4, 2)),
+    ('1.0.0b3', (1, 0, 0, -3, 3)),
+    ('1.0.0rc4', (1, 0, 0, -2, 4)),
+    ('0.0.0', (0, 0, 0, -1)),
+    ('0.0.11', (0, 0, 11, -1)),
+    ('0.11.0', (0, 11, 0, -1)),
+    ('11.0.0', (11, 0, 0, -1)),
+])
+def test_format_version(version_str, version_tuple):
+    assert format_version(version_tuple) == version_str

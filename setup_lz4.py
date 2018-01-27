9cf8d57f51738bcc570d042192cacb6a83039865
Thomas Waldmann <tw@waldmann-edv.de>
2018-01-27 19:49:42 +0100

    do .h file content checks in binary mode, fixes #3544
    we can't know the encoding header file authors will use,
    so using binary for our simple checks is the safest way.

--- setup_lz4.py
+++ setup_lz4.py
@@ -24,8 +24,8 @@ def lz4_system_prefix(prefixes):
     for prefix in prefixes:
         filename = os.path.join(prefix, 'include', 'lz4.h')
         if os.path.exists(filename):
-            with open(filename, 'r') as fd:
-                if 'LZ4_compress_default' in fd.read():  # requires lz4 >= 1.7.0 (r129)
+            with open(filename, 'rb') as fd:
+                if b'LZ4_compress_default' in fd.read():  # requires lz4 >= 1.7.0 (r129)
                     return prefix
 
 

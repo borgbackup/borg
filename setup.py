5be756a0c6d526fc513fa0ad08767b127b941e90
Thomas Waldmann <tw@waldmann-edv.de>
2018-04-12 23:35:38 +0200

    update msgpack requirement, fixes #3753

--- setup.py
+++ setup.py
@@ -36,10 +36,15 @@ if my_python < min_python:
 on_rtd = os.environ.get('READTHEDOCS')
 
 install_requires = [
-    # msgpack pure python data corruption was fixed in 0.4.6.
-    # msgpack 0.5.0 was a bit of a troublemaker.
-    # also, msgpack dropped py34 support at 0.5.0.
-    'msgpack-python>=0.4.6,<0.5.0',
+    # we are rather picky about msgpack versions, because a good working msgpack is
+    # very important for borg, see https://github.com/borgbackup/borg/issues/3753
+    # best versions seem to be 0.4.6, 0.4.7, 0.4.8 and 0.5.6:
+    'msgpack-python >=0.4.6, <=0.5.6, !=0.5.0, !=0.5.1, !=0.5.2, !=0.5.3, !=0.5.4, !=0.5.5',
+    # if you can't satisfy the above requirement, these are versions that might
+    # also work ok, IF you make sure to use the COMPILED version of msgpack-python,
+    # NOT the PURE PYTHON fallback implementation: ==0.5.1, ==0.5.4
+    # using any other version is not supported by borg development, feel free to
+    # do it on your own risk (and after own testing).
 ]
 
 # note for package maintainers: if you package borgbackup for distribution,

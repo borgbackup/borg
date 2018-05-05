0405c1a1bba5d800a445152eb8b1e27cea4be255
Thomas Waldmann <tw@waldmann-edv.de>
2018-05-05 14:56:13 +0200

    fix borg config --cache (minimal change)
    this was broken by previous changeset.
    
    now doing just a minimal change for better diff display,
    see also next commit.
    
    (cherry picked from commit 697bb3640228915d95260070c4d769a0e703e9a9)

--- src/borg/archiver.py
+++ src/borg/archiver.py
@@ -1574,6 +1574,11 @@ class Archiver:
             name = args.name
 
         if args.cache:
+            manifest, key = Manifest.load(repository, (Manifest.Operation.WRITE,))
+            assert_secure(repository, manifest)
+            cache = Cache(repository, key, manifest, lock_wait=self.lock_wait)
+
+        if args.cache:
             cache.cache_config.load()
             config = cache.cache_config._config
             save = cache.cache_config.save
@@ -1601,6 +1606,9 @@ class Archiver:
             except (configparser.NoOptionError, configparser.NoSectionError) as e:
                 print(e, file=sys.stderr)
                 return EXIT_WARNING
+
+        if args.cache:
+            cache.close()
         return EXIT_SUCCESS
 
     def do_debug_info(self, args):

b76661ab67a766a1a81d6f191affff03b7bc2a11
Thomas Waldmann <tw@waldmann-edv.de>
2018-07-05 22:56:54 +0200

    acl platform code: fix acl set return type (master)

--- src/borg/platform/darwin.pyx
+++ src/borg/platform/darwin.pyx
@@ -12,7 +12,7 @@ cdef extern from "sys/acl.h":
 
     int acl_free(void *obj)
     acl_t acl_get_link_np(const char *path, int type)
-    acl_t acl_set_link_np(const char *path, int type, acl_t acl)
+    int acl_set_link_np(const char *path, int type, acl_t acl)
     acl_t acl_from_text(const char *buf)
     char *acl_to_text(acl_t acl, ssize_t *len_p)
     int ACL_TYPE_EXTENDED

751cd2caedf8d59857c0a4ebc76e54b04196e342
Thomas Waldmann <tw@waldmann-edv.de>
2018-08-06 17:48:27 +0200

    add python 3.7 to pypi metadata

--- setup.py
+++ setup.py
@@ -277,6 +277,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Security :: Cryptography',
         'Topic :: System :: Archiving :: Backup',
     ],

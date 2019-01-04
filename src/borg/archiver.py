4e72095e4b24ae922d1734721384b3b4cf5b6ae2
cornop <12619914+cornop@users.noreply.github.com>
2019-01-04 16:18:10 +0100

    Clarify the necessity to separately backup key in repokey mode

--- src/borg/archiver.py
+++ src/borg/archiver.py
@@ -258,11 +258,18 @@ class Archiver:
                 'for details about the security implications.', shlex.quote(path))
 
         if key.NAME != 'plaintext':
-            logger.warning(
-                '\n'
-                'IMPORTANT: you will need both KEY AND PASSPHRASE to access this repo!\n'
-                'Use "borg key export" to export the key, optionally in printable format.\n'
-                'Write down the passphrase. Store both at safe place(s).\n')
+            if 'repokey' in key.NAME:
+                logger.warning(
+                    '\n'
+                    'IMPORTANT: you will need both KEY AND PASSPHRASE to access this repo!\n'
+                    'The key is already included in the repository config.\n'
+                    'Write down the passphrase. Store it at safe place(s).\n')
+            else:
+                logger.warning(
+                    '\n'
+                    'IMPORTANT: you will need both KEY AND PASSPHRASE to access this repo!\n'
+                    'Use "borg key export" to export the key, optionally in printable format.\n'
+                    'Write down the passphrase. Store both at safe place(s).\n')
         return self.exit_code
 
     @with_repository(exclusive=True, manifest=False)

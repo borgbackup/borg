f828e883bdd8fbbb07b3bf6d7cf710c61ce5e8a7
Thomas Waldmann <tw@waldmann-edv.de>
2019-03-13 13:45:19 +0100

    travis: install script improvements

--- .travis/run.sh
+++ .travis/run.sh
@@ -5,14 +5,13 @@ set -x
 
 if [[ "$(uname -s)" == "Darwin" ]]; then
     eval "$(pyenv init -)"
-    if [[ "${OPENSSL}" != "0.9.8" ]]; then
-        # set our flags to use homebrew openssl
-        export ARCHFLAGS="-arch x86_64"
-        export LDFLAGS="-L/usr/local/opt/openssl/lib"
-        export CFLAGS="-I/usr/local/opt/openssl/include"
-    fi
+    # set our flags to use homebrew openssl
+    export ARCHFLAGS="-arch x86_64"
+    export PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig:$PKG_CONFIG_PATH"
+    #export LDFLAGS="-L/usr/local/opt/openssl/lib"
+    #export CFLAGS="-I/usr/local/opt/openssl/include"
 fi
 
 # do not use fakeroot, but run as root on travis.
 # avoids the dreaded EISDIR sporadic failures. see #2482.
-sudo bash -c "source ~/.venv/bin/activate ; tox -e $TOXENV -r"
+sudo -E bash -c "source ~/.venv/bin/activate ; tox -e $TOXENV -r"

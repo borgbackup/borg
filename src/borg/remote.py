26ead19036a89fb7da58917d9033e75fe204a346
Thalian <github@fantasya-pbem.de>
2019-10-22 20:20:10 +0200

    [FEATURE] #4674 - Add compact threshold option
    Set previous threshold in @api decorator to 0.1.

--- src/borg/remote.py
+++ src/borg/remote.py
@@ -900,7 +900,7 @@ This problem will go away as soon as the server has been upgraded to 1.0.7+.
 
     @api(since=parse_version('1.0.0'),
          compact={'since': parse_version('1.2.0a0'), 'previously': True, 'dontcare': True},
-         threshold={'since': parse_version('1.2.0a0'), 'previously': 0, 'dontcare': True},
+         threshold={'since': parse_version('1.2.0a0'), 'previously': 0.1, 'dontcare': True},
          cleanup_commits={'since': parse_version('1.2.0a0'), 'previously': False, 'dontcare': True})
     def commit(self, save_space=False, compact=True, threshold=0.1, cleanup_commits=False):
         """actual remoting is done via self.call in the @api decorator"""

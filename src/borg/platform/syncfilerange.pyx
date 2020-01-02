242424ecae1fe083166a71b241abeb74f8232ae1
Will <wshaos@posteo.net>
2020-01-02 12:54:00 -0500

    Move sync_file_range to its own extension

---
+++ src/borg/platform/syncfilerange.pyx
@@ -0,0 +1,13 @@
+from libc.stdint cimport int64_t
+
+
+# Some Linux systems (like Termux on Android 7 or earlier) do not have access
+# to sync_file_range. By isolating the access to sync_file_range in this
+# separate extension, it can be imported dynamically from linux.pyx only when
+# available and systems without support can otherwise use the rest of
+# linux.pyx.
+cdef extern from "fcntl.h":
+    int sync_file_range(int fd, int64_t offset, int64_t nbytes, unsigned int flags)
+    unsigned int SYNC_FILE_RANGE_WRITE
+    unsigned int SYNC_FILE_RANGE_WAIT_BEFORE
+    unsigned int SYNC_FILE_RANGE_WAIT_AFTER

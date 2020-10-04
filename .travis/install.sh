023d69082b89339a63dc958543a9ea7c0850278a
ernestoarbitrio <ernesto.arbitrio@gmail.com>
2020-10-04 15:04:45 +0200

    re-locate brew update in the correct placce

--- .travis/install.sh
+++ .travis/install.sh
@@ -11,8 +11,8 @@ then
 
     # Update brew itself
     export HOMEBREW_NO_AUTO_UPDATE=1  # Auto-updating everything would take too much time
-    brew cleanup  # Preempt possible scheduled clean-up so it doesn't clutter the log later
     brew update > /dev/null
+    brew cleanup  # Preempt possible scheduled clean-up so it doesn't clutter the log later
     # Install and/or upgrade dependencies
     brew install lz4 || brew upgrade lz4
     brew install xz || brew upgrade xz  # Required for Python lzma module

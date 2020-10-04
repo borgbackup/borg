7dc161067498703dca27b6e2437548a9cc4d981b
Thomas Waldmann <tw@waldmann-edv.de>
2020-10-04 20:36:31 +0200

    selecting least broken llfuse version
    1.3.6: works on all supported OSes, but not on py39
    1.3.7: does not work on FreeBSD, but with py39

--- setup.py
+++ setup.py
@@ -83,8 +83,8 @@ install_requires = [
 # if you do not have llfuse, do not require it, most of borgbackup will work.
 extras_require = {
     'fuse': [
-        'llfuse >=1.3, <2.0',
-        'llfuse >=1.3.4, <2.0; python_version >="3.7"',
+        'llfuse >=1.3.4, <1.3.7; python_version <"3.9"',  # broken on py39
+        'llfuse >=1.3.7, <2.0; python_version >="3.9"',  # broken on freebsd
     ],
 }
 

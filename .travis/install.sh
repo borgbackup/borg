1a1985cec9487ab26de4de4ad5d1365beb4b21e5
ernestoarbitrio <ernesto.arbitrio@gmail.com>
2020-10-06 15:45:08 +0200

    address last CR comments

--- .travis/install.sh
+++ .travis/install.sh
@@ -26,12 +26,8 @@ then
     eval "$(pyenv init -)"
     if [ "${TOXENV}" = "py35" ]
     then
-        pyenv install 3.5.3
+        pyenv install 3.5.3 #v3.5.0 just won't compile with openssl 1.1.
         pyenv global 3.5.3
-    elif [ "${TOXENV}" = "py36" ]
-    then
-        pyenv install 3.6.0
-        pyenv global 3.6.0
     elif [ "${TOXENV}" = "py37" ]
     then
         pyenv install 3.7.0

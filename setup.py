6c82cb186f34dc47d9005b34b345d39c3582e876
ernestoarbitrio <ernesto.arbitrio@gmail.com>
2020-10-06 12:15:57 +0200

    remove conditional branch from setup.py

--- setup.py
+++ setup.py
@@ -63,9 +63,6 @@ if sys.platform.startswith('freebsd'):
     # llfuse 0.41.1, 1.1 are ok
     extras_require['fuse'] = ['llfuse <2.0, !=0.42.*, !=0.43, !=1.0', ]
 
-# if my_python >= (3, 7):
-#     extras_require['fuse'][0] += ', >=1.3.4'
-
 from setuptools import setup, find_packages, Extension
 from setuptools.command.sdist import sdist
 from distutils.command.clean import clean

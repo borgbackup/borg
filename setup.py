eb4092fba5d9f4702c8e11e3ea53ce95ab0d5b78
Thomas Waldmann <tw@waldmann-edv.de>
2020-10-09 03:07:28 +0200

    relax libxxhash version requirement
    i tested it with libxxhash 0.6.5 on debian buster [amd64], it worked.
    
    but for ARM it requires an important fix that was released in 0.7.2.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ prefer_system_libzstd = True
 # True: use the shared libb2 from the system, False: use the bundled blake2 code
 prefer_system_libb2 = True
 
-# True: use the shared libxxhash (>= 0.7.4) from the system, False: use the bundled xxhash code
+# True: use the shared libxxhash (>= 0.6.5 [>= 0.7.2 on ARM]) from the system, False: use the bundled xxhash code
 prefer_system_libxxhash = True
 
 # prefer_system_msgpack is another option, but you need to set it in src/borg/helpers.py.

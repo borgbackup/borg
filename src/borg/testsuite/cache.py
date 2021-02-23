2211b840a3f11fef319e643b7d5b57b8e0f5432e
Thomas Waldmann <tw@waldmann-edv.de>
2021-02-23 22:56:38 +0100

    verbose files cache logging via --debug-topic=files_cache, fixes #5659

--- src/borg/testsuite/cache.py
+++ src/borg/testsuite/cache.py
@@ -256,7 +256,7 @@ class TestAdHocCache:
             repository.get(H(5))
 
     def test_files_cache(self, cache):
-        assert cache.file_known_and_unchanged(bytes(32), None) == (False, None)
+        assert cache.file_known_and_unchanged(b'foo', bytes(32), None) == (False, None)
         assert cache.cache_mode == 'd'
         assert cache.files is None
 

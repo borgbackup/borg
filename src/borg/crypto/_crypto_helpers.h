aa57a3cb57d8a4682a64564743fe7d769a1b3c29
Thomas Waldmann <tw@waldmann-edv.de>
2022-03-20 22:38:59 +0100

    remove AEAD ciphers AES-OCB and CHACHA20-POLY1305, fixes #6472
    not used in 1.2.x, we'll start using them in 1.3.

--- src/borg/crypto/_crypto_helpers.h
+++ src/borg/crypto/_crypto_helpers.h
@@ -10,12 +10,6 @@ void HMAC_CTX_free(HMAC_CTX *ctx);
 #endif
 
 
-#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
-const EVP_CIPHER *EVP_aes_256_ocb(void);  /* dummy, so that code compiles */
-const EVP_CIPHER *EVP_chacha20_poly1305(void);  /* dummy, so that code compiles */
-#endif
-
-
 #if !defined(LIBRESSL_VERSION_NUMBER)
 #define LIBRESSL_VERSION_NUMBER 0
 #endif

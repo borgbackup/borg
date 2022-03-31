28ae76d59084a54a74442236339f3ec93cb82090
Andrey Bienkowski <hexagon-recursion@posteo.net>
2022-03-31 08:51:31 +0300

    Fix the testsuite/key.py again

--- src/borg/testsuite/key.py
+++ src/borg/testsuite/key.py
@@ -25,7 +25,7 @@ from ..helpers import msgpack
 class TestKey:
     class MockArgs:
         location = Location(tempfile.mkstemp()[1])
-        key_algorithm = "argon2 aes256-ctr hmac-sha256"
+        key_algorithm = "argon2"
 
     keyfile2_key_file = """
         BORG_KEY 0000000000000000000000000000000000000000000000000000000000000000

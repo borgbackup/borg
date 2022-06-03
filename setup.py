6d024961aa52f2721a3c57aa3b5fe5bc35a6c8ee
Thomas Waldmann <tw@waldmann-edv.de>
2022-06-03 10:01:50 +0200

    add python 3.11 to pypi metadata

--- setup.py
+++ setup.py
@@ -285,6 +285,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Security :: Cryptography',
         'Topic :: System :: Archiving :: Backup',
     ],

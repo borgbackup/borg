2d0e590be5baa2fdadc72e6a5fbb7d8ee79474a4
Michael Deyaso <mdeyaso@fusioniq.io>
2023-03-22 14:58:56 +0300

    prune --keep-all: alias for --keep-last <inf>, fixes #6656

--- src/borg/archiver/prune_cmd.py
+++ src/borg/archiver/prune_cmd.py
@@ -138,7 +138,7 @@ class PruneMixIn:
                 'At least one of the "keep-within", "keep-last", '
                 '"keep-secondly", "keep-minutely", "keep-hourly", "keep-daily", '
                 '"keep-weekly", "keep-monthly", "keep-13weekly", "keep-3monthly", '
-                'or "keep-yearly" settings must be specified.'
+                '"keep-yearly", or "keep-all" settings must be specified.'
             )
 
         if args.format is not None:
@@ -319,6 +319,13 @@ class PruneMixIn:
             default=0,
             action=Highlander,
             help="number of secondly archives to keep",
+        )
+        subparser.add_argument(
+            "--keep-all",
+            dest="secondly",
+            action="store_const",
+            const=float("inf"),
+            help="keep all archives (alias of --keep-last=<infinite>)",
         )
         subparser.add_argument(
             "--keep-minutely",

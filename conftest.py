d46aeaa3b5bf01d518fca470823911ca6ddda81a
bigtedde <big.tedde@gmail.com>
2023-06-23 19:44:01 -0700

    all archiver base test case methods converted

--- conftest.py
+++ conftest.py
@@ -1,16 +1,23 @@
+import errno
 import io
 import os
+import stat
 import subprocess
 import sys
+import time
+from configparser import ConfigParser
 from io import StringIO, BytesIO
 
 import pytest
 
 # needed to get pretty assertion failures in unit tests:
-from borg import helpers
+from borg import helpers, platform, xattr
 from borg.archive import Archive
-from borg.constants import EXIT_SUCCESS
+from borg.cache import Cache
+from borg.constants import EXIT_SUCCESS, CACHE_TAG_NAME, CACHE_TAG_CONTENTS
+from borg.helpers import bin_to_hex
 from borg.manifest import Manifest
+from borg.platformflags import is_win32
 from borg.repository import Repository
 
 if hasattr(pytest, "register_assert_rewrite"):
@@ -24,7 +31,7 @@ from borg.logger import setup_logging, flush_logging  # noqa: E402
 # Ensure that the loggers exist for all tests
 setup_logging()
 
-from borg.testsuite import has_lchflags, has_llfuse, has_pyfuse3  # noqa: E402
+from borg.testsuite import has_lchflags, has_llfuse, has_pyfuse3, are_fifos_supported, changedir  # noqa: E402
 from borg.testsuite import are_symlinks_supported, are_hardlinks_supported, is_utime_fully_supported  # noqa: E402
 from borg.testsuite.platform import fakeroot_detected  # noqa: E402
 
@@ -93,7 +100,7 @@ def requires_hardlinks():
 
 
 @pytest.fixture(autouse=True)
-def set_env_variables(monkeypatch):
+def set_env_variables():
     os.environ["BORG_CHECK_I_KNOW_WHAT_I_AM_DOING"] = "YES"
     os.environ["BORG_DELETE_I_KNOW_WHAT_I_AM_DOING"] = "YES"
     os.environ["BORG_PASSPHRASE"] = "waytooeasyonlyfortests"
@@ -248,10 +255,262 @@ def create_src_archive(cmd_fixture, archiver_setup):
 def open_archive(archiver_setup):
     def archive_and_repository(name):
         repo_path = archiver_setup.repository_path
-        repository = Repository(repo_path, exclusive=True, create=True)
+        repository = Repository(repo_path, exclusive=True)
         with repository:
             manifest = Manifest.load(repository, Manifest.NO_OPERATION_CHECK)
             archive = Archive(manifest, name)
         return archive, repository
 
     return archive_and_repository
+
+
+@pytest.fixture()
+def open_repository(self):
+    def repo():
+        return Repository(self.repository_path, exclusive=True)
+
+    return repo
+
+
+@pytest.fixture()
+def create_regular_file(archiver_setup):
+    def regular_file(name, size=0, contents=None):
+        assert not (size != 0 and contents and len(contents) != size), "size and contents do not match"
+        filename = os.path.join(str(archiver_setup.input_path), str(name))
+        if not os.path.exists(os.path.dirname(filename)):
+            os.makedirs(os.path.dirname(filename))
+        with open(filename, "wb") as fd:
+            if contents is None:
+                contents = b"X" * size
+            fd.write(contents)
+
+    return regular_file
+
+
+@pytest.fixture()
+def create_test_files(archiver_setup, create_regular_file):
+    def test_files(create_hardlinks=True):
+        # Create a minimal test case including all supported file types
+        input_path = str(archiver_setup.input_path)
+
+        # File
+        create_regular_file("file1", size=1024 * 80)
+        create_regular_file("flagfile", size=1024)
+        # Directory
+        create_regular_file("dir2/file2", size=1024 * 80)
+        # File mode
+        os.chmod("input/file1", 0o4755)
+        # Hard link
+        if are_hardlinks_supported() and create_hardlinks:
+            os.link(os.path.join(input_path, "file1"), os.path.join(input_path, "hardlink"))
+        # Symlink
+        if are_symlinks_supported():
+            os.symlink("somewhere", os.path.join(input_path, "link1"))
+        create_regular_file("fusexattr", size=1)
+        if not xattr.XATTR_FAKEROOT and xattr.is_enabled(archiver_setup.input_path):
+            fn = os.fsencode(os.path.join(input_path, "fusexattr"))
+            # ironically, due to the way how fakeroot works, comparing FUSE file xattrs to orig file xattrs
+            # will FAIL if fakeroot supports xattrs, thus we only set the xattr if XATTR_FAKEROOT is False.
+            # This is because fakeroot with xattr-support does not propagate xattrs of the underlying file
+            # into "fakeroot space". Because the xattrs exposed by borgfs are these of an underlying file
+            # (from fakeroots point of view) they are invisible to the test process inside the fakeroot.
+            xattr.setxattr(fn, b"user.foo", b"bar")
+            xattr.setxattr(fn, b"user.empty", b"")
+            # XXX this always fails for me
+            # ubuntu 14.04, on a TMP dir filesystem with user_xattr, using fakeroot
+            # same for newer ubuntu and centos.
+            # if this is supported just on specific platform, platform should be checked first,
+            # so that the test setup for all tests using it does not fail here always for others.
+        # FIFO node
+        if are_fifos_supported():
+            os.mkfifo(os.path.join(input_path, "fifo1"))
+        if has_lchflags:
+            platform.set_flags(os.path.join(input_path, "flagfile"), stat.UF_NODUMP)
+
+        if is_win32:
+            have_root = False
+        else:
+            try:
+                # Block device
+                os.mknod("input/bdev", 0o600 | stat.S_IFBLK, os.makedev(10, 20))
+                # Char device
+                os.mknod("input/cdev", 0o600 | stat.S_IFCHR, os.makedev(30, 40))
+                # File owner
+                os.chown("input/file1", 100, 200)  # raises OSError invalid argument on cygwin
+                # File mode
+                os.chmod("input/dir2", 0o555)  # if we take away write perms, we need root to remove contents
+                have_root = True  # we have (fake)root
+            except PermissionError:
+                have_root = False
+            except OSError as e:
+                # Note: ENOSYS "Function not implemented" happens as non-root on Win 10 Linux Subsystem.
+                if e.errno not in (errno.EINVAL, errno.ENOSYS):
+                    raise
+                have_root = False
+        time.sleep(1)  # "empty" must have newer timestamp than other files
+        create_regular_file("empty", size=0)
+        return have_root
+
+    return test_files
+
+
+@pytest.fixture()
+def _extract_repository_id(archiver_setup):
+    def extract_repo_id():
+        with Repository(archiver_setup.repository_path) as repository:
+            return repository.id
+
+    return extract_repo_id
+
+
+@pytest.fixture()
+def _set_repository_id(archiver_setup):
+    def set_id(path, id):
+        config = ConfigParser(interpolation=None)
+        config.read(os.path.join(path, "config"))
+        config.set("repository", "id", bin_to_hex(id))
+        with open(os.path.join(path, "config"), "w") as fd:
+            config.write(fd)
+        with Repository(archiver_setup.repository_path) as repository:
+            return repository.id
+
+    return set_id
+
+
+@pytest.fixture()
+def _extract_hardlinks_setup(archiver_setup, create_regular_file, cmd_fixture):
+    def extract_hardlinks_setup():
+        input_path = str(archiver_setup.input_path)
+        repository_location = archiver_setup.repository_location
+
+        os.mkdir(os.path.join(input_path, "dir1"))
+        os.mkdir(os.path.join(input_path, "dir1/subdir"))
+
+        create_regular_file("source", contents=b"123456")
+        os.link(os.path.join(input_path, "source"), os.path.join(input_path, "abba"))
+        os.link(os.path.join(input_path, "source"), os.path.join(input_path, "dir1/hardlink"))
+        os.link(os.path.join(input_path, "source"), os.path.join(input_path, "dir1/subdir/hardlink"))
+
+        create_regular_file("dir1/source2")
+        os.link(os.path.join(input_path, "dir1/source2"), os.path.join(input_path, "dir1/aaaa"))
+
+        cmd_fixture(f"--repo={repository_location}", "rcreate", archiver_setup.RK_ENCRYPTION)
+        cmd_fixture(f"--repo={repository_location}", "create", "test", "input")
+
+    return extract_hardlinks_setup
+
+
+@pytest.fixture()
+def _create_test_caches(archiver_setup, cmd_fixture, create_regular_file):
+    def create_test_caches():
+        cmd_fixture(f"--repo={archiver_setup.repository_location}", "rcreate", archiver_setup.RK_ENCRYPTION)
+        create_regular_file("file1", size=1024 * 80)
+        create_regular_file("cache1/%s" % CACHE_TAG_NAME, contents=CACHE_TAG_CONTENTS + b" extra stuff")
+        create_regular_file("cache2/%s" % CACHE_TAG_NAME, contents=b"invalid signature")
+        os.mkdir("input/cache3")
+        if are_hardlinks_supported():
+            os.link("input/cache1/%s" % CACHE_TAG_NAME, "input/cache3/%s" % CACHE_TAG_NAME)
+        else:
+            create_regular_file("cache3/%s" % CACHE_TAG_NAME, contents=CACHE_TAG_CONTENTS + b" extra stuff")
+
+    return create_test_caches
+
+
+@pytest.fixture()
+def _assert_test_caches(archiver_setup, cmd_fixture):
+    def assert_test_caches():
+        with changedir("output"):
+            cmd_fixture(f"--repo={archiver_setup.repository_location}", "extract", "test")
+        assert sorted(os.listdir("output/input")) == ["cache2", "file1"]
+        assert sorted(os.listdir("output/input/cache2")) == [CACHE_TAG_NAME]
+
+    return assert_test_caches
+
+
+@pytest.fixture()
+def _create_test_tagged(archiver_setup, cmd_fixture, create_regular_file):
+    def create_test_tagged():
+        cmd_fixture(f"--repo={archiver_setup.repository_location}", "rcreate", archiver_setup.RK_ENCRYPTION)
+        create_regular_file("file1", size=1024 * 80)
+        create_regular_file("tagged1/.NOBACKUP")
+        create_regular_file("tagged2/00-NOBACKUP")
+        create_regular_file("tagged3/.NOBACKUP/file2", size=1024)
+
+    return create_test_tagged
+
+
+@pytest.fixture()
+def _assert_test_tagged(archiver_setup, cmd_fixture):
+    def assert_test_tagged():
+        with changedir("output"):
+            cmd_fixture(f"--repo={archiver_setup.repository_location}", "extract", "test")
+        assert sorted(os.listdir("output/input")) == ["file1"]
+
+    return assert_test_tagged
+
+
+@pytest.fixture()
+def _create_test_keep_tagged(archiver_setup, cmd_fixture, create_regular_file):
+    def create_test_keep_tagged():
+        cmd_fixture(f"--repo={archiver_setup.repository_location}", "rcreate", archiver_setup.RK_ENCRYPTION)
+        create_regular_file("file0", size=1024)
+        create_regular_file("tagged1/.NOBACKUP1")
+        create_regular_file("tagged1/file1", size=1024)
+        create_regular_file("tagged2/.NOBACKUP2/subfile1", size=1024)
+        create_regular_file("tagged2/file2", size=1024)
+        create_regular_file("tagged3/%s" % CACHE_TAG_NAME, contents=CACHE_TAG_CONTENTS + b" extra stuff")
+        create_regular_file("tagged3/file3", size=1024)
+        create_regular_file("taggedall/.NOBACKUP1")
+        create_regular_file("taggedall/.NOBACKUP2/subfile1", size=1024)
+        create_regular_file("taggedall/%s" % CACHE_TAG_NAME, contents=CACHE_TAG_CONTENTS + b" extra stuff")
+        create_regular_file("taggedall/file4", size=1024)
+
+    return create_test_keep_tagged
+
+
+@pytest.fixture()
+def _assert_test_keep_tagged(archiver_setup, cmd_fixture):
+    def assert_test_keep_tagged():
+        with changedir("output"):
+            cmd_fixture(f"--repo={archiver_setup.repository_location}", "extract", "test")
+        assert sorted(os.listdir("output/input")) == ["file0", "tagged1", "tagged2", "tagged3", "taggedall"]
+        assert os.listdir("output/input/tagged1") == [".NOBACKUP1"]
+        assert os.listdir("output/input/tagged2") == [".NOBACKUP2"]
+        assert os.listdir("output/input/tagged3") == [CACHE_TAG_NAME]
+        assert sorted(os.listdir("output/input/taggedall")) == [".NOBACKUP1", ".NOBACKUP2", CACHE_TAG_NAME]
+
+    return assert_test_keep_tagged
+
+
+@pytest.fixture()
+def check_cache(archiver_setup, cmd_fixture, open_repository):
+    # First run a regular borg check
+    cmd_fixture(f"--repo={archiver_setup.repository_location}", "check")
+    # Then check that the cache on disk matches exactly what's in the repo.
+    with open_repository() as repository:
+        manifest = Manifest.load(repository, Manifest.NO_OPERATION_CHECK)
+        with Cache(repository, manifest, sync=False) as cache:
+            original_chunks = cache.chunks
+        Cache.destroy(repository)
+        with Cache(repository, manifest) as cache:
+            correct_chunks = cache.chunks
+    assert original_chunks is not correct_chunks
+    seen = set()
+    for id, (refcount, size) in correct_chunks.iteritems():
+        o_refcount, o_size = original_chunks[id]
+        assert refcount == o_refcount
+        assert size == o_size
+        seen.add(id)
+    for id, (refcount, size) in original_chunks.iteritems():
+        assert id in seen
+
+
+@pytest.fixture()
+def remote_prefix(archiver_setup):
+    archiver_setup.prefix = "ssh://__testsuite__"
+
+
+@pytest.fixture()
+def archiver_binary_base(archiver_setup):
+    archiver_setup.EXE = "borg.exe"
+    archiver_setup.FORK_DEFAULT = True

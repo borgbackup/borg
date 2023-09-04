915e58653b1b60100ee9ad29a009ce08d572f946
Thomas Waldmann <tw@waldmann-edv.de>
2023-09-04 22:15:42 +0200

    replace flake8 by ruff

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,10 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-import sys, os
-sys.path.insert(0, os.path.abspath('../src'))
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath("../src"))
 
 from borg import __version__ as sw_version
 

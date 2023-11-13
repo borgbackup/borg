c6bde98943ead80e6b96caa29a3f96822c215248
Thomas Waldmann <tw@waldmann-edv.de>
2023-11-13 20:31:46 +0100

    shorten TAMRequiredError error msg
    Users using recently created repos (after borg 1.0.9) or
    who followed the upgrade procedure of 1.0.9 or >1.2.4 will
    never see this error msg.
    
    So, have it as short as and similar to the ArchiveTAMRequiredError.

--- src/borg/crypto/key.py
+++ src/borg/crypto/key.py
@@ -90,11 +90,6 @@ class RepoKeyNotFoundError(Error):
 class TAMRequiredError(IntegrityError):
     __doc__ = textwrap.dedent("""
     Manifest is unauthenticated, but it is required for this repository.
-
-    This either means that you are under attack, or that you modified this repository
-    with a Borg version older than 1.0.9 after TAM authentication was enabled.
-
-    In the latter case, use "borg upgrade --tam --force '{}'" to re-authenticate the manifest.
     """).strip()
     traceback = True
     exit_mcode = 98

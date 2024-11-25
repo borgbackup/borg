c429decbee826a84cb6c11744cdfa36c0c78624d
Thomas Waldmann <tw@waldmann-edv.de>
2024-11-25 21:55:30 +0100

    simplify fetch_many api
    it now takes either:
    - a list of ChunkListEntry namedtuples
    - a list of chunk IDs [bytes]

--- src/borg/archiver/tar_cmds.py
+++ src/borg/archiver/tar_cmds.py
@@ -113,9 +113,7 @@ class TarMixIn:
             """
             Return a file-like object that reads from the chunks of *item*.
             """
-            chunk_iterator = archive.pipeline.fetch_many(
-                [chunk_id for chunk_id, _ in item.chunks], is_preloaded=True, ro_type=ROBJ_FILE_STREAM
-            )
+            chunk_iterator = archive.pipeline.fetch_many(item.chunks, is_preloaded=True, ro_type=ROBJ_FILE_STREAM)
             if pi:
                 info = [remove_surrogates(item.path)]
                 return ChunkIteratorFileWrapper(

0b284db33a84bca46b2127ad20d80ba912535add
Thomas Waldmann <tw@waldmann-edv.de>
2025-04-15 18:09:00 +0200

    fix remote repository exception handling / exit codes, fixes #8631
    also: reduce code duplication.

--- src/borg/testsuite/archiver/return_codes_test.py
+++ src/borg/testsuite/archiver/return_codes_test.py
@@ -1,6 +1,11 @@
+import os
+
 from ...constants import *  # NOQA
 from ...helpers import IncludePatternNeverMatchedWarning
-from . import cmd_fixture, changedir  # NOQA
+from ...repository import Repository
+from . import cmd, cmd_fixture, changedir, generate_archiver_tests  # NOQA
+
+pytest_generate_tests = lambda metafunc: generate_archiver_tests(metafunc, kinds="local,remote,binary")  # NOQA
 
 
 def test_return_codes(cmd_fixture, tmpdir):
@@ -17,3 +22,14 @@ def test_return_codes(cmd_fixture, tmpdir):
         assert rc == EXIT_SUCCESS
     rc, out = cmd_fixture("--repo=%s" % repo, "extract", "archive", "does/not/match")
     assert rc == IncludePatternNeverMatchedWarning().exit_code
+
+
+def test_exit_codes(archivers, request, tmpdir, monkeypatch):
+    archiver = request.getfixturevalue(archivers)
+    # we create the repo path, but do NOT initialize the borg repo,
+    # so the borg create commands are expected to fail with DoesNotExist (was: InvalidRepository in borg 1.4).
+    os.makedirs(archiver.repository_path)
+    monkeypatch.setenv("BORG_EXIT_CODES", "classic")
+    cmd(archiver, "create", "archive", "input", fork=True, exit_code=EXIT_ERROR)
+    monkeypatch.setenv("BORG_EXIT_CODES", "modern")
+    cmd(archiver, "create", "archive", "input", fork=True, exit_code=Repository.DoesNotExist.exit_mcode)

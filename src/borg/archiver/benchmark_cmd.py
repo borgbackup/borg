5445a52994a24e39341929d02fdaac39a25c16ef
Thomas Waldmann <tw@waldmann-edv.de>
2025-05-27 23:21:58 +0200

    Chunker: add sparse/fmap support

--- src/borg/archiver/benchmark_cmd.py
+++ src/borg/archiver/benchmark_cmd.py
@@ -146,7 +146,7 @@ class BenchmarkMixIn:
                     pass
 
         for spec, func in [
-            ("buzhash,19,23,21,4095", lambda: chunkit("buzhash", 19, 23, 21, 4095, seed=0)),
+            ("buzhash,19,23,21,4095", lambda: chunkit("buzhash", 19, 23, 21, 4095, seed=0, sparse=False)),
             ("fixed,1048576", lambda: chunkit("fixed", 1048576, sparse=False)),
         ]:
             print(f"{spec:<24} {size:<10} {timeit(func, number=100):.3f}s")

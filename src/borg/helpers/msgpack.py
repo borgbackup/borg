da58d92aa6a62f85baf377933c74a079c8c3faa9
Thomas Waldmann <tw@waldmann-edv.de>
2025-10-09 13:16:20 +0200

    allow msgpack 1.1.2
    1.1.2 provides wheels for py 3.14.

--- src/borg/helpers/msgpack.py
+++ src/borg/helpers/msgpack.py
@@ -210,7 +210,7 @@ def is_supported_msgpack():
 
     if msgpack.version in []:  # < add bad releases here to deny list
         return False
-    return (1, 0, 3) <= msgpack.version[:3] <= (1, 1, 1)
+    return (1, 0, 3) <= msgpack.version[:3] <= (1, 1, 2)
 
 
 def get_limited_unpacker(kind):

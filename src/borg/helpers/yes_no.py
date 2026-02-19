b2861b2645f1c27b657363b79c70ad822d9f03d8
Daxmeister <113617175+Daxmeister@users.noreply.github.com>
2026-02-19 09:33:16 +0100

    [FEAT] Coverage checker for yes /src/borg/helpers/yes_no.py #22
    Closes #2
    
    Co-authored-by: Davide Mario Attebrant Sbrzesny <dmas@kth.se>

--- src/borg/helpers/yes_no.py
+++ src/borg/helpers/yes_no.py
@@ -3,6 +3,7 @@ import json
 import os
 import os.path
 import sys
+from borg.helpers.coverage_diy import register, mark
 
 FALSISH = ("No", "NO", "no", "N", "n", "0")
 TRUISH = ("Yes", "YES", "yes", "Y", "y", "1")
@@ -60,6 +61,33 @@ def yes(
     :param input: input function [input from builtins]
     :return: boolean answer value, True or False
     """
+    
+    # Setup for DIY coverage
+    YES_BRANCHES = [
+        "YES_01_invalid_default",
+        "YES_02_ofile_none",
+                "YES_03_answer_is_error",
+        "YES_04_msg_printed",
+        "YES_05_env_override_set",
+        "YES_06_env_override_none",
+        "YES_07_env_answer_used",
+        "YES_08_env_answer_none",
+        "YES_09_prompt_false_return_default",
+        "YES_10_input_success",
+        "YES_11_input_error_eof",
+        "YES_12_input_error_unicode",
+        "YES_13_answer_in_defaultish",
+        "YES_14_default_msg_printed",
+        "YES_15_answer_in_truish",
+        "YES_16_true_msg_printed",
+        "YES_17_answer_in_falsish",
+        "YES_18_false_msg_printed",
+        "YES_19_invalid_answer_retry",
+        "YES_20_invalid_answer_no_retry",
+        "YES_21_retry_msg_printed",
+    ]
+    for bid in YES_BRANCHES:
+        register(bid)
 
     def output(msg, msg_type, is_prompt=False, **kwargs):
         json_output = getattr(logging.getLogger("borg"), "json", False)
@@ -76,47 +104,72 @@ def yes(
     # note: we do not assign sys.stderr as the default above, so it is
     # really evaluated NOW, not at function definition time.
     if ofile is None:
+        mark("YES_02_ofile_none")
         ofile = sys.stderr
     if default not in (True, False):
+        mark("YES_01_invalid_default")
         raise ValueError("invalid default value, must be True or False")
     if msg:
+        mark("YES_04_msg_printed")
         output(msg, "prompt", is_prompt=True)
     while True:
         answer = None
         if env_var_override:
+            mark("YES_05_env_override_set")
             answer = os.environ.get(env_var_override)
             if answer is not None and env_msg:
+                mark("YES_07_env_answer_used")
                 output(env_msg.format(answer, env_var_override), "env_answer", env_var=env_var_override)
+            else:
+                mark("YES_08_env_answer_none")
+        else:
+            mark("YES_06_env_override_none")
+        
         if answer is None:
             if not prompt:
+                mark("YES_09_prompt_false_return_default")
                 return default
             try:
                 answer = input()  # this may raise UnicodeDecodeError, #6984
+                mark("YES_10_input_success")
                 if answer == ERROR:  # for testing purposes
+                    mark("YES_03_answer_is_error")
                     raise UnicodeDecodeError("?", b"?", 0, 1, "?")  # args don't matter
             except EOFError:
                 # avoid defaultish[0], defaultish could be empty
+                mark("YES_11_input_error_eof")
                 answer = truish[0] if default else falsish[0]
             except UnicodeDecodeError:
+                mark("YES_12_input_error_unicode")
                 answer = ERROR
+                
         if answer in defaultish:
+            mark("YES_13_answer_in_defaultish")
             if default_msg:
+                mark("YES_14_default_msg_printed")
                 output(default_msg, "accepted_default")
             return default
         if answer in truish:
+            mark("YES_15_answer_in_truish")
             if true_msg:
+                mark("YES_16_true_msg_printed")
                 output(true_msg, "accepted_true")
             return True
         if answer in falsish:
+            mark("YES_17_answer_in_falsish")
             if false_msg:
+                mark("YES_18_false_msg_printed")
                 output(false_msg, "accepted_false")
             return False
         # if we get here, the answer was invalid
         if invalid_msg:
+            mark("YES_19_invalid_answer_retry")
             output(invalid_msg, "invalid_answer")
         if not retry:
+            mark("YES_20_invalid_answer_no_retry")
             return default
         if retry_msg:
+            mark("YES_21_retry_msg_printed")
             output(retry_msg, "prompt_retry", is_prompt=True)
         # in case we used an environment variable and it gave an invalid answer, do not use it again:
         env_var_override = None
